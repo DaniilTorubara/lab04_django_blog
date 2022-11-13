c9721df89b9c69289cc4576823e90cb89d4ac36c
daniil-torubara <dabarara@bk.ru>
2022-11-13 23:41:30 +0300

    commit 1

---
+++ blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include('the_blog.urls')),
+    path('authentication/', include('django.contrib.auth.urls')),
+    path('authentication/', include('authentication.urls')),
+]
